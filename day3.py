5597aa1c2cef80ca4701e0907e495c955da0f383
ghanekarpayal11213 <126890237+ghanekarpayal11213@users.noreply.github.com>
2023-03-03 20:02:09 +0530

    DAY 3 ASSIGNMENT

---
+++ day3.py
@@ -0,0 +1,13 @@
+#input the principal amount ,time and rate
+principal = float(input("Enter the principal amount:"))
+time =float(input("Enter the time in years:"))
+rate = float(input("Enter the rate in interest:"))
+
+
+#calculate the simple interest
+simple_interest = (principal * time * rate)/100
+
+#print the result
+print("the simple interest is:",simple_interest)
+
+  
